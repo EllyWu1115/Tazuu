365396e5cb2641540606d8d1a77a61cf6c3c3c04
elly <lian1115.lw@gmial.com>
2021-11-14 13:30:54 +0800

    second time commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ActionController::Base
   def sign_up
+    @user = User.new
+  end
+
+  def account_verify
+    clean_params = params.require(:user).permit(:username, :password, :email)
+
+    @user = User.new(clean_params)
+
+
+
+  if @user.save
+    redirect_to "/"
+  else
+    # redirect_to "/sign_up"   render的用意是當網頁輸入錯誤 下一次不需要再全部重新填寫一次 會保留輸入值
+    render :sign_up
+
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/sign_up", to: "users#sign_up"
+  get "/account_verify", to: "users#account_verify"
 end
